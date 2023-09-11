ec0e55b90e8c3f43d2af35de920b6dffcda20b4a
Алексей Столяров <71269791+complex-tube@users.noreply.github.com>
2023-09-11 13:55:45 +0300

    Do card animation event

---
+++ src/app/main/main.module.ts
@@ -0,0 +1,20 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { CardsListComponent } from './cards-list/cards-list.component';
+import { CardItemComponent } from './cards-list/card-item/card-item.component';
+
+
+
+@NgModule({
+  declarations: [
+    CardsListComponent,
+    CardItemComponent
+  ],
+  exports: [
+    CardsListComponent
+  ],
+  imports: [
+    CommonModule
+  ]
+})
+export class MainModule { }
